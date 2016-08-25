11ec2eefc70badf9ea0687f714dfb781c0bfb34b
Fernando Rodriguez <frodriguez.developer@outlook.com>
2016-08-25 15:21:47 -0400

    Adjust buffer sizes
    MPEG movies where locking up before the first frame is displayed. This
    commit fixes that by upping the video buffer size.

--- src/player.c
+++ src/player.c
@@ -51,9 +51,9 @@
 #define MB_DECODER_PIX_FMT 		(AV_PIX_FMT_BGRA)
 
 /* This is the # of frames to decode ahead of time */
-#define MB_VIDEO_BUFFER_FRAMES  (15)
+#define MB_VIDEO_BUFFER_FRAMES  (50)
 #define MB_VIDEO_BUFFER_PACKETS (1)
-#define MB_AUDIO_BUFFER_FRAMES  (60)
+#define MB_AUDIO_BUFFER_FRAMES  (160)
 #define MB_AUDIO_BUFFER_PACKETS (1)
 
 #define MB_DECODER_PRINT_FPS
