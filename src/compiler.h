8fd1d0c4c05b041b3192fcb08e16e8551d345f32
Fernando Rodriguez <frodzdev@gmail.com>
2016-10-14 14:55:55 -0400

    android: Make branch optimizations optional
    This commit adds the --enable-branch-optimization configure option. It
    is useful for testing with and without the optimizations and for
    disabling it in order to use profile guided optimizations.

--- src/compiler.h
+++ src/compiler.h
@@ -1,9 +1,18 @@
 #ifndef __MB_COMPILER__
 #define __MB_COMPILER__
 
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
 /* Macros for optimizing likely branches */
+#ifdef ENABLE_BRANCH_OPTIMIZATION
 #define LIKELY(x)               (__builtin_expect(!!(x), 1))
 #define UNLIKELY(x)             (__builtin_expect(!!(x), 0))
+#else
+#define LIKELY(x)		(x)
+#define UNLIKELY(x)		(x)
+#endif
 
 
 #define ATOMIC_INC(addr) (__sync_fetch_and_add(addr, 1))
