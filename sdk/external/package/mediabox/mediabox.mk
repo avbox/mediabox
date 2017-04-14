0531d4d3b34af62b083aef6d7dab961213ebfcf4
Fernando Rodriguez <frodzdev@gmail.com>
2017-04-14 12:08:54 -0400

    sdk: Update build system

--- sdk/external/package/mediabox/mediabox.mk
+++ sdk/external/package/mediabox/mediabox.mk
@@ -9,17 +9,22 @@ MEDIABOX_SITE = https://bitbucket.org/frodzdev/mediabox.git
 MEDIABOX_SITE_METHOD = git
 MEDIABOX_LICENSE = OTHER
 MEDIABOX_LICENSE_FILES = COPYING
-MEDIABOX_INSTALL_STAGING = YES
+MEDIABOX_INSTALL_STAGING = NO
 MEDIABOX_AUTORECONF = YES
-
-MEDIABOX_CONF_OPTS = \
-	--enable-debug \
-	--without-systemd
-
 MEDIABOX_DEPENDENCIES = libcurl libupnp directfb openssl mediatomb avmount
+
+MEDIABOX_CONF_OPTS = --without-systemd
+
+ifeq ($(BR2_PACKAGE_MEDIABOX_DEBUG),y)
+MEDIABOX_CONF_OPTS += --enable-debug
+endif
 
 ifeq ($(BR2_PACKAGE_MEDIABOX_BLUETOOTH),y)
 MEDIABOX_CONF_OPTS += --enable-bluetooth
+endif
+
+ifeq ($(BR2_PACKAGE_MEDIABOX_LIBDRM),y)
+MEDIABOX_CONF_OPTS += --enable-libdrm
 endif
 
 $(eval $(autotools-package))
