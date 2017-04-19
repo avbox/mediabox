07242c9915206713db2f7d70a778ae8bfa9e23ca
Fernando Rodriguez <frodzdev@gmail.com>
2017-04-18 22:48:50 -0400

    mediabox: Add /proc/cmdline parser
    When running as pid 1 use any kernel parameters prefixed with mediabox.
    instead of the arguments supplied by main().

--- src/main.c
+++ src/main.c
@@ -15,6 +15,7 @@
 #include <signal.h>
 #include <pwd.h>
 #include <sys/types.h>
+#include <sys/mount.h>
 #include <asoundlib.h>
 
 #define LOG_MODULE "main"
@@ -69,6 +70,122 @@ signal_handler(int signum)
 
 
 /**
+ * Parse kernel arguments
+ */
+static char **
+parse_kernel_args(int *argc)
+{
+#define CMDLINE_MAX	(1024)
+#define ARGS_MAX	(10)
+
+	int fd, i = 0, n = 0;
+	char buf[CMDLINE_MAX];
+	char **argv, *arg;
+	ssize_t nb_read;
+
+	/* we need to make sure /proc is mounted before we
+	 * can read /proc/cmdline */
+	if (mount("proc", "/proc", "proc", 0, "") == -1) {
+		return NULL;
+	}
+
+	/* read the file */
+	if ((fd = open("/proc/cmdline", O_RDONLY)) == -1) {
+		return NULL;
+	}
+	if ((nb_read = read(fd, buf, CMDLINE_MAX-2)) < 0 || nb_read  == CMDLINE_MAX-2) {
+		close(fd);
+		if (nb_read == CMDLINE_MAX-2) {
+			errno = E2BIG;
+		}
+		return NULL;
+	}
+	close(fd);
+
+	/* make sure command-line is NULL terminate and replace
+	 * the ending newline with a space so we terminate the
+	 * last argument before exitting the loop */
+	assert(nb_read >= 1 && buf[nb_read - 1] == '\n');
+	buf[nb_read - 1] = ' '; /* ensure we're closed at end of loop */
+	buf[nb_read + 0] = '\0';
+
+	/* allocate heap memory for arguments list and the
+	 * first argument (program name) */
+	if ((argv = malloc((ARGS_MAX+1) * sizeof(char*))) == NULL) {
+		return NULL;
+	} else {
+		memset(argv, 0, (ARGS_MAX+1) * sizeof(char*));
+	}
+	if ((argv[0] = strdup(prog)) == NULL) {
+		return NULL;
+	}
+	*argc = 1;
+
+	/* parse all arguments prefixed with 'mediabox.' */
+	while (i < nb_read) {
+		if (buf[i] != ' ' && buf[i] != '\n') {
+			if (n == 0) {
+				arg = &buf[i];
+			}
+			n++;
+		} else {
+			buf[i] = '\0';
+			if (n > 0) {
+				if (*argc < ARGS_MAX && !strncmp("mediabox.", arg, 9)) {
+					if ((argv[*argc] = malloc((n - 6) * sizeof(char))) == NULL) {
+						goto err;
+					}
+
+					sprintf(argv[*argc], "--%s", arg + 9);
+					assert((n - 6) > strlen(argv[*argc]));
+					(*argc)++;
+				} else {
+					if (*argc >= ARGS_MAX) {
+						fprintf(stderr, "%s: Too many arguments! This build "
+							"only supports %d kernel arguments!!",
+							prog, ARGS_MAX);
+						errno = E2BIG;
+						goto err;
+					}
+				}
+				n = 0;
+			}
+		}
+		i++;
+	}
+
+	return argv;
+err:
+	for (i = 0; i < ARGS_MAX; i++) {
+		if (argv[i] != NULL) {
+			free(argv[i]);
+		}
+	}
+	free(argv);
+	return NULL;
+
+#undef ARGS_MAX
+#undef CMDLINE_MAX
+}
+
+
+/**
+ * Free the list of kernel arguments.
+ */
+static void
+free_kernel_args(int argc, char **argv)
+{
+	int i;
+	for (i = 0; i < argc; i++) {
+		assert(argv[i] != NULL);
+		free(argv[i]);
+	}
+	assert(argv[i] == NULL);
+	free(argv);
+}
+
+
+/**
  * Print usage.
  */
 static void
@@ -78,6 +195,7 @@ print_usage()
 	printf("\n");
 	printf(" --version\t\tPrint version information\n");
 	printf(" --no-direct\t\tDon't use direct rendering\n");
+	printf(" --video:driver=<drv>\tSet the video driver string\n");
 	printf(" --dont-launch-avmount\tAssume avmount is already mounted\n");
 	printf(" --dont-launch-mediatomb\tDon't launch mediatomb\n");
 	printf(" --dfb:XXX\t\tDirectFB options. See directfbrc(5)\n");
@@ -88,21 +206,34 @@ print_usage()
 
 
 int
-main (int argc, char **argv)
+main (int argc, char **cargv)
 {
 	int i;
 	int launch_avmount = 1;
 	int launch_mediatomb = 1;
 	int init;
 	char *progmem, *logfile = NULL;
+	char **argv;
 
 	init = (getpid() == 1);
 
 	/* save program name */
-	if ((progmem = strdup(argv[0])) == NULL) {
-		prog = argv[0];
+	if ((progmem = strdup(cargv[0])) == NULL) {
+		prog = cargv[0];
 	} else {
 		prog = basename(progmem);
+	}
+
+	/* if we're running as pid 1 parse arguments from
+	 * kernel */
+	if (init) {
+		if ((argv = parse_kernel_args(&argc)) == NULL) {
+			fprintf(stderr, "%s: Cannot parse kernel args: %s (%i)",
+				prog, strerror(errno), errno);
+			exit(EXIT_FAILURE);
+		}
+	} else {
+		argv = cargv;
 	}
 
 	/* parse command line */
@@ -141,7 +272,7 @@ main (int argc, char **argv)
 	 * logging */
 	log_init();
 
-	/* if not logfile was specified and we're running
+	/* if no logfile was specified and we're running
 	 * as init then use the default */
 	if (init && logfile == NULL) {
 		logfile = "/var/log/mediabox.log";
@@ -222,6 +353,12 @@ main (int argc, char **argv)
 		signal(SIGINT, signal_handler) == SIG_ERR) {
 		LOG_PRINT_ERROR("Could not set signal handlers");
 		exit(EXIT_FAILURE);
+	}
+
+	/* if we're running as pid 1 free the list
+	 * of kernel arguments */
+	if (init) {
+		free_kernel_args(argc, argv);
 	}
 
 	/* show the shell */
