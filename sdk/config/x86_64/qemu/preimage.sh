82e67eb5ff3476afc362f1a8e59d2e97b289d0d1
Fernando Rodriguez <frodzdev@gmail.com>
2017-04-20 12:42:36 -0400

    sdk: Cleanup config files

--- sdk/config/x86_64/qemu/preimage.sh
+++ sdk/config/x86_64/qemu/preimage.sh
@@ -11,8 +11,33 @@ if [ -e "${DFB_SYSTEMS}/libdirectfb_dummy.so" ]; then
 	rm -v "${DFB_SYSTEMS}/libdirectfb_dummy.so";
 fi
 
+# junk
+rm -rvf output/target/opt
+rm -rvf output/target/usr/bin/compile-et.pl
+rm -rvf output/target/usr/bin/gettex*
+rm -rvf output/target/usr/bin/gapplication
+rm -rvf output/target/usr/bin/pango-view
+rm -rvf output/target/usr/bin/ngettext
+rm -rvf output/target/usr/bin/hb-*
+rm -rvf output/target/usr/bin/gsettings
+rm -rvf output/target/usr/bin/gresource
+rm -rvf output/target/usr/bin/js-config
+rm -rvf output/target/usr/bin/nspr-config
+
+
+# directfb files
 rm -rvf output/target/usr/bin/dfbplay
 rm -rvf output/target/usr/bin/dfbshow
+rm -rvf output/target/usr/bin/dfbswitch
+
+# libevdev
+rm -rvf output/target/usr/bin/libevdev-*
+rm -rvf output/target/usr/bin/mtdev-test
+
+# libinput
+rm -rvf output/target/usr/bin/libinput-*
+rm -rvf output/target/usr/bin/touchpad-*
+rm -rvf output/target/usr/bin/mouse-dpi-tool
 
 # fix libmozjs symlinks. TODO: This needs to be done
 # by spidermonkey's makefile
@@ -20,10 +45,14 @@ ln -sf libmozjs185.so.1.0.0 output/target/usr/lib/libmozjs185.so
 ln -sf libmozjs185.so.1.0.0 output/target/usr/lib/libmozjs185.so.1.0
 
 
+# remove unneeded deluge files
 rm -rvf output/target/usr/bin/deluge
 rm -rvf output/target/usr/bin/deluge-gtk
 rm -rvf output/target/usr/bin/deluge-web
 ln -s deluge-console output/target/usr/bin/deluge
+
+# remove fontconfig files
+rm -rvf output/target/usr/bin/fc-*
 
 # create log directory (by default it's a symlink to /tmp!)
 rm -rvf output/target/var/log
