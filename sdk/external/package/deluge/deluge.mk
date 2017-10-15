e572690c4cdd8433f295255b2e13951642811251
Fernando Rodriguez <frodzdev@gmail.com>
2017-10-15 03:05:09 -0400

    sdk: Fix deluge build errors
    Add patch to prevent deluge's setup script from accessing the local
    library paths.

--- sdk/external/package/deluge/deluge.mk
+++ sdk/external/package/deluge/deluge.mk
@@ -10,7 +10,8 @@ DELUGE_SOURCE = deluge-${DELUGE_VERSION}.tar.bz2
 DELUGE_DEPENDENCIES = python host-python python-setuptools \
 	python-pyxdg python-service-identity python-chardet \
 	python-attrs \
-	python-pyopenssl libtorrent-rasterbar
+	python-pyopenssl libtorrent-rasterbar \
+	boost
 DELUGE_SETUP_TYPE = setuptools
 DELUGE_LICENSE = GPLv2
 DELUGE_LICENSE_FILES = COPYING
