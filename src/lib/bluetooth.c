ebb5d638400dbf293e57784cf65b924398118444
Fernando Rodriguez <frodzdev@gmail.com>
2017-10-30 13:23:15 -0400

    Change license to GPLv3

--- src/lib/bluetooth.c
+++ src/lib/bluetooth.c
@@ -1,10 +1,25 @@
 /**
- * Copyright (c) 2016-2017 Fernando Rodriguez - All rights reserved
- * This file is part of mediabox.
+ * avbox - Toolkit for Embedded Multimedia Applications
+ * Copyright (C) 2016-2017 Fernando Rodriguez
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License Version 3 as 
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
  */
 
+
 #ifdef HAVE_CONFIG_H
-#include "config.h"
+#	include "../config.h"
 #endif
 #ifdef ENABLE_BLUETOOTH
 #include <stdlib.h>
--- src/lib/ui/input.h
+++ src/lib/ui/input.h
@@ -1,7 +1,22 @@
 /**
- * Copyright (c) 2016-2017 Fernando Rodriguez - All rights reserved
- * This file is part of mediabox.
+ * avbox - Toolkit for Embedded Multimedia Applications
+ * Copyright (C) 2016-2017 Fernando Rodriguez
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License Version 3 as 
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
  */
+
 
 #ifndef __MB_INPUT_H__
 #define __MB_INPUT_H__
