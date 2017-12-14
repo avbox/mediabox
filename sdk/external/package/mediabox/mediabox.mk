3fb5fb9492754027cb45f5e297a8c9795dc43d30
Fernando Rodriguez <frodzdev@gmail.com>
2017-12-14 14:39:43 -0500

    sdk: Add config files for the Raspberry Pi Zero

--- sdk/external/package/mediabox/mediabox.mk
+++ sdk/external/package/mediabox/mediabox.mk
@@ -14,7 +14,7 @@ MEDIABOX_LICENSE = OTHER
 MEDIABOX_LICENSE_FILES = COPYING
 MEDIABOX_INSTALL_STAGING = NO
 MEDIABOX_AUTORECONF = YES
-MEDIABOX_DEPENDENCIES = libcurl libupnp directfb openssl mediatomb avmount bluealsa
+MEDIABOX_DEPENDENCIES = libcurl libupnp directfb openssl mediatomb avmount
 
 MEDIABOX_CONF_OPTS = --without-systemd
 
@@ -23,6 +23,7 @@ MEDIABOX_CONF_OPTS += --enable-debug
 endif
 
 ifeq ($(BR2_PACKAGE_MEDIABOX_BLUETOOTH),y)
+MEDIABOX_DEPENDENCIES += bluealsa
 MEDIABOX_CONF_OPTS += --enable-bluetooth
 endif
 
